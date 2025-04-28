8a7240052d673aa0b9052cfa8544ee9c6ef6b3d9
Mimosa Joenväärä <mimosa.joenvaara@student.oulu.fi>
2025-04-28 22:04:06 +0300

    miau

--- src/pages/Laskuri.js
+++ src/pages/Laskuri.js
@@ -1,7 +1,7 @@
 export default function Laskuri() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Tervetuloa budjettilaskuriin!</h1>
+      <h1 className="title">LASKULASKURI</h1>
       {/* Lisää tänne myöhemmin laskuri-toimintoja */}
     </div>
   );
